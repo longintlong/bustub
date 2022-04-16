b0020fae84296eac8931c8259ac9c49717e4763c
longintlong <747099367@qq.com>
2022-04-16 12:55:24 +0000

    project task 1: LRU_Replacer

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,63 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+    size_ = 0;
+    capacity_ = num_pages;
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if(!size_) return false;
+    *frame_id = lru_list_.back();
+    lru_map_.erase(*frame_id);
+    lru_list_.pop_back();
+    --size_;
+    return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    LRU_Remove(frame_id);
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    LRU_Append(frame_id);
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    int ret = size_;
+    return ret;
+}
+
+void LRUReplacer::LRU_Append(frame_id_t frame_id) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    // frame is already in lru replacer
+    if(lru_map_.count(frame_id)) {
+        return;
+    }
+    lru_list_.push_front(frame_id);
+    lru_map_[frame_id] = lru_list_.begin();
+    ++size_;
+}
+
+void LRUReplacer::LRU_Remove(frame_id_t frame_id) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    if(!lru_map_.count(frame_id)) {
+        return;
+    }
+    lru_list_.erase(lru_map_[frame_id]);
+    lru_map_.erase(frame_id);
+    --size_;
+}
+
+void LRUReplacer::MoveToHead(frame_id_t frame_id) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    lru_list_.erase(lru_map_[frame_id]);
+    lru_list_.push_front(frame_id);
+    lru_map_[frame_id] = lru_list_.begin();
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -14,6 +14,7 @@
 
 #include <list>
 #include <mutex>  // NOLINT
+#include <unordered_map>
 #include <vector>
 
 #include "buffer/replacer.h"
@@ -47,6 +48,19 @@ class LRUReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+  void LRU_Append(frame_id_t frame_id);
+
+  void LRU_Remove(frame_id_t frame_id);
+
+  void MoveToHead(frame_id_t frame_id);
+
+  size_t capacity_;
+
+  // mutex_ protects the following state
+  std::mutex mutex_;
+  std::list<frame_id_t> lru_list_;
+  std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> lru_map_;
+  size_t size_;
 };
 
 }  // namespace bustub
